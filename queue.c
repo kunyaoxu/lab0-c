6c677e8d4bf32cc6cb24fcbd66b342f074a676eb
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-03-02 18:42:47 +0000

    Let q_remove_head function work
    Return false if queue is NULL or empty.
    If sp is non-NULL and an element is removed, copy the removed string to
    *sp.
    Free the list element and the string.
    Let q->size minus one.

--- queue.c
+++ queue.c
@@ -93,7 +93,19 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
+    if (sp != NULL) {
+        strncpy(sp, q->head->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    if (q->size == 0) {
+        return false;
+    }
+    list_ele_t *head;
+    head = q->head;
     q->head = q->head->next;
+    q->size -= 1;
+    free(head->value);
+    free(head);
     return true;
 }
 

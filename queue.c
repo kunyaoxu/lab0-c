fe687be72c33a1a294111c680d483afedce9bd03
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-03-02 17:48:50 +0000

    Let q_insert_head function work
    If parameter q is null, return false.
    If q parameter not null, the code will allocate space for the string
    and copy it, assign value to new node, add one to q->size and return
    true.

--- queue.c
+++ queue.c
@@ -48,13 +48,22 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (q == NULL) {
+        return false;
+    }
     list_ele_t *newh;
+    char *str;
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    str = malloc(strlen(s) + 1);
+    memset(str, 0, strlen(s) + 1);
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+    strncpy(str, s, strlen(s) + 1);
+    newh->value = str;
     newh->next = q->head;
     q->head = newh;
+    q->size += 1;
     return true;
 }
 
